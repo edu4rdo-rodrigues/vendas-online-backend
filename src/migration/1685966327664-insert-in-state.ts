c1b04aef023f946c1c95806576bac0dad273ea4a
edu4rdo-rodrigues <eduardo28455@gmail.com>
2023-06-07 12:23:20 -0300

    feat: insert migration in project4

---
+++ src/migration/1685966327664-insert-in-state.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class InsertInState1685966327664 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
---
+++ src/user/user.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDto } from 'src/user/dtos/createUser.dto';
+import { UserEntity } from 'src/user/interfaces/user.entity';
+import { UserService } from 'src/user/user.service';
+
+@Controller('user')
+export class UserController {
+
+  constructor(private readonly userService: UserService) {} 
+
+  
+  @Post('post')
+  async createUser(@Body() createUser : CreateUserDto): Promise<UserEntity> {
+    return this.userService.createUser(createUser);
+  }
+
+
+  @Get('get')
+  async getAllUsers(): Promise<UserEntity[]> {
+    return this.userService.getAllUser(); //JSON.stringify({ test: 'abc' });
+  }
+}
